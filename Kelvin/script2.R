5af67e72a6f964cb7b549ab30a9f2d52d28d4437
Djruhnke <Djruhnke@email.wm.edu>
2014-09-28 13:07:53 -0400

    Daniel's Bid Data

--- Kelvin/script2.R
+++ Kelvin/script2.R
@@ -1,5 +1,11 @@
-setwd('/users/kelvinabrokwa/documents/repositories/wm-stats-blog/kelvin')
+install.packages('googleVis')
+library(googleVis)
+
+setwd('C:\Users\djruh_000\Documents\GitHub\wm-stats-blog\Kelvin\')
+
 data.long <- read.csv('agg_data_long.csv', header=TRUE)
+
+
 
 motion = gvisMotionChart(data.long, idvar="variable", timevar="Year")
 plot(motion)
