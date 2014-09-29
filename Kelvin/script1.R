066c23abfb5382f113d28cb4685546b6c95edb9f
kelvinabrokwa <kelvinabrokwa@Kelvins-MacBook-Pro.local>
2014-09-28 23:00:41 -0400

    Update

--- Kelvin/script1.R
+++ Kelvin/script1.R
@@ -1,5 +1,5 @@
 setwd('/users/kelvinabrokwa/documents/repositories/wm-stats-blog/kelvin')
-data.wide <- read.csv('agg_data_2.csv', header=TRUE)
+data.wide <- read.csv('agg_data_4.csv', header=TRUE)
 
 data.wide$Year <- factor(data.wide$Year)
 View(data.wide)
@@ -8,7 +8,6 @@ library(reshape2)
 
 data.long <- melt(data.wide, id.vars=c("Year"))
 View(data.long)
-
 
 motion = gvisMotionChart(data.long, idvar="variable", timevar="Year")
 plot(motion)
