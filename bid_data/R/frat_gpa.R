f570c15800df266b8949824e3e846d10ff96f19c
kelvinabrokwa <kelvinabrokwa@Kelvins-MacBook-Pro.local>
2014-11-02 11:03:16 -0500

    Graphics Code

---
+++ bid_data/R/frat_gpa.R
@@ -0,0 +1,26 @@
+setwd('/users/kelvinabrokwa/documents/repositories/wm-stats-blog/bid_data/raw_data/')
+
+data = read.csv('frat_gpa2.csv',header=TRUE,check.names=FALSE)
+
+data_df <- data.frame(data,check.names=FALSE)
+df <- data_df[c(1,2,4,5,8,21)]
+
+line_frat <- gvisLineChart(df, xvar='Time', yvar=colnames(df[-1]),
+                      options=list(
+                        title='Fraternity GPA',
+                        fontSize=11,
+                        vAxis.viewWindowMode='pretty',
+                        hAxis="[{textStyle:7}]",
+                        width=600,
+                        height=400))
+
+plot(line_frat)
+
+# Highest Mean GPA
+# AEPi: 3.294
+# Beta: 3.269
+# Sig Ep: 3.322
+# ATO: 3.257
+# Delta Phi: 3.191
+
+# Note seasonal GPA fluctuation
