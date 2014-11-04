8d92d1ef61015449c4d9325aedb07cf52e44bd6a
kelvinabrokwa <kelvinabrokwa@Kelvins-MacBook-Pro.local>
2014-11-04 13:36:41 -0500

    frat gpa code cleaned

--- bid_data/R/frat_gpa.R
+++ bid_data/R/frat_gpa.R
@@ -2,7 +2,6 @@ setwd('/users/kelvinabrokwa/documents/repositories/wm-stats-blog/bid_data/raw_da
 
 library(googleVis)
 
-<<<<<<< HEAD
 data = read.csv('frat_gpa2.csv',header=TRUE,check.names=FALSE)
 
 data_df <- data.frame(data,check.names=FALSE)
@@ -17,32 +16,13 @@ line_frat <- gvisLineChart(df, xvar='Time', yvar=colnames(df[-1]),
                         height=400))
 
 plot(line_frat)
-=======
-data = read.csv('frat_gpa2.csv',header=TRUE)
 
-data_df <- data.frame(data)
-df <- data_df[c(1,2,4,5,8,21)]
-
-line <- gvisLineChart(df, xvar='Time', yvar=colnames(df[-1]),
-                      options=list(
-                        title='Fraternity GPA',
-                        fontSize=12,
-                        vAxis.viewWindowMode='pretty',
-                        hAxis.textStyle={fontSize:7},
-                        width=600,
-                        height=400))
-plot(line)
->>>>>>> FETCH_HEAD
 
 # Highest Mean GPA
 # AEPi: 3.294
 # Beta: 3.269
 # Sig Ep: 3.322
 # ATO: 3.257
-<<<<<<< HEAD
 # Delta Phi: 3.191
-=======
-# Delphi Phi: 3.191
->>>>>>> FETCH_HEAD
 
 # Note seasonal GPA fluctuation
